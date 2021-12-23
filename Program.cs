fec3b931fac713baa12559635686da7d000bdbd1
eduard <kuchmel.eded@mail.ru>
2021-12-23 03:01:57 +0300

    Added working playing module without enumeration of cards function

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace MyApp
     {
         public static void Main(string[] args)
         {
+
             List<Card> deck = new List<Card>();
             string CardSuit = "червей";
             CardEditor(deck, CardSuit);
@@ -18,13 +19,13 @@ namespace MyApp
             CardSuit = "крести";
             CardEditor(deck, CardSuit);
 
-            Console.WriteLine($"Ваша карта {deck[51].Rank} {deck[51].Suit}");
-            
-          
+            //Console.WriteLine($"Ваша карта {deck[51].Rank} {deck[51].Suit}");
+            PlayingModule(deck);
+
             Console.ReadKey();
         }
         //переменная isdeckshort создана для возможности игры колодой из 36 карт
-        public static void CardEditor (List <Card> methodDeckName, string methodCardSuit, bool isdeckshort=false)
+        public static void CardEditor(List<Card> methodDeckName, string methodCardSuit, bool isdeckshort = false)
         {
             methodDeckName.Add(new Card() { Rank = "двойка", Suit = methodCardSuit, NumValue = 2 });
             methodDeckName.Add(new Card() { Rank = "тройка", Suit = methodCardSuit, NumValue = 3 });
@@ -39,6 +40,49 @@ namespace MyApp
             methodDeckName.Add(new Card() { Rank = "дама", Suit = methodCardSuit, NumValue = 3 });
             methodDeckName.Add(new Card() { Rank = "король", Suit = methodCardSuit, NumValue = 4 });
             methodDeckName.Add(new Card() { Rank = "туз", Suit = methodCardSuit, NumValue = 11 });
+        }
+        public static void PlayingModule(List<Card> methodDeckName)
+        {
+            Random rng = new Random();
+            string requestMessageTemplate = "Хотите ли вы вытянуть еще одну карту?";
+            int playerScore = 0;
+            while (true)
+            {
+                
+                int randomDeckCardNumber = rng.Next(methodDeckName.Count);
+                даConsole.WriteLine($"Ваша карта {methodDeckName[randomDeckCardNumber].Rank} {methodDeckName[randomDeckCardNumber].Suit}");
+                playerScore = playerScore + methodDeckName[randomDeckCardNumber].NumValue;
+                methodDeckName.RemoveAt(randomDeckCardNumber);
+                Console.WriteLine($"Ваш текущий счет = {playerScore}");
+                if (!GetUserChoice(requestMessageTemplate))
+                {
+                    Console.WriteLine($"Игра завершена, ваш итоговый счет - {playerScore}");
+                    return;     
+                }
+            }
+        }
+        public static bool GetUserChoice(string requestMessage)
+        {
+           
+            while (true)
+            {
+
+                Console.WriteLine(requestMessage);
+                string userAnswer = Console.ReadLine();
+                if (userAnswer.ToLower() == "да")
+                {
+                    return true;
+                }
+                else if (userAnswer.ToLower() == "нет")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Хуйню ввел, попробуй еще раз");
+                }
+            }
+
         }
     }
     public class Card
