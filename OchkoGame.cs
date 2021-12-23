8c77f98be771a3a7b15241c00fd3101cc28ff447
eduard <kuchmel.eded@mail.ru>
2021-12-24 00:12:19 +0300

    Added multiplayer system (draft)

--- OchkoGame.cs
+++ OchkoGame.cs
@@ -14,12 +14,11 @@ namespace MyApp
             List<Card> deck = new List<Card>();
             //заполнение колоды двумя методами
             AddDifferentCardSuitsToDeck(deck);
-            //Вызов игрового модуля
             OchkoPlaying(deck);
             Console.ReadKey();
         }
         //Метод для создания колоды (задает численное значение и номинал карты)
-        public static void AddDifferentCardsToDeck (List<Card> methodDeckName, string methodCardSuit)
+        public static void AddDifferentCardsToDeck(List<Card> methodDeckName, string methodCardSuit)
         {
             methodDeckName.Add(new Card() { Rank = "двойка", Suit = methodCardSuit, NumValue = 2 });
             methodDeckName.Add(new Card() { Rank = "тройка", Suit = methodCardSuit, NumValue = 3 });
@@ -35,7 +34,7 @@ namespace MyApp
             methodDeckName.Add(new Card() { Rank = "король", Suit = methodCardSuit, NumValue = 4 });
             methodDeckName.Add(new Card() { Rank = "туз", Suit = methodCardSuit, NumValue = 11 });
         }
-        public static void AddDifferentCardSuitsToDeck (List <Card> methodDeckName)
+        public static void AddDifferentCardSuitsToDeck(List<Card> methodDeckName)
         {
             string CardSuit = "червей";
             AddDifferentCardsToDeck(methodDeckName, CardSuit);
@@ -49,40 +48,84 @@ namespace MyApp
         //Игровой модуль
         public static void OchkoPlaying(List<Card> methodDeckName)
         {
+            //ввод пользователей в игру
+            List<Player> players = new List<Player>();
+            Console.WriteLine("Введите количество игроков");
+            int playerCount = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= playerCount - 1; i++)
+            {
+                Console.WriteLine($"Введите имя {i + 1}-ого игрока");
+                players.Add(new Player() { Name = Console.ReadLine() });
+            }
+
             //рандомизация раздач
             Random rng = new Random();
             //пример предложения о вытягивании карты
-            string requestMessageTemplate = "Хотите ли вы вытянуть еще одну карту?";
-            int playerScore = 0;
-            while (true)
+            string requestMessageTemplate = ", хотите ли вы вытянуть еще одну карту?";
+            int playersInGame = playerCount;
+            int[] winners = new int[playerCount];
+            for (int i = 0; i < playerCount; i++)
             {
                 int randomDeckCardNumber = rng.Next(methodDeckName.Count);
-                Console.WriteLine($"Ваша карта {methodDeckName[randomDeckCardNumber].Rank} {methodDeckName[randomDeckCardNumber].Suit}");
-                playerScore = playerScore + methodDeckName[randomDeckCardNumber].NumValue;
-                if (playerScore > 21)
-                {
-                    Console.WriteLine($"Вы проиграли, ваш итоговый счет - {playerScore}");
-                    return;
-                }
-                else if (playerScore == 21)
-                {
-                    Console.WriteLine("Вы победили, набрав 21 очко (молодец нахуй)");
-                    return;
-                }
+                Console.WriteLine($"{players[i].Name}, ваша карта {methodDeckName[randomDeckCardNumber].Rank} {methodDeckName[randomDeckCardNumber].Suit}");
+                players[i].Score = players[i].Score + methodDeckName[randomDeckCardNumber].NumValue;
                 methodDeckName.RemoveAt(randomDeckCardNumber);
-                Console.WriteLine($"Ваш текущий счет = {playerScore}");
-                if (!GettingUserChoice(requestMessageTemplate))
+            }
+            while (playersInGame >= 1)
+            {
+
+                for (int i = 0; i < playerCount; i++)
                 {
-                    Console.WriteLine($"Игра завершена, ваш итоговый счет - {playerScore}");
-                    return;
+                    if (players[i].Score == 0)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine($"{players[i].Name}, ваш текущий счет = {players[i].Score}");
+                    if (!GettingUserChoice(players[i].Name, requestMessageTemplate))
+                    {
+                        Console.WriteLine($"{players[i].Name}, игра завершена, ваш итоговый счет - {players[i].Score}");
+                      
+                        winners[i] = players[i].Score;
+                        players[i].Score = 0;
+                        playersInGame = playersInGame - 1;
+                        continue;
+                    }
+                    int randomDeckCardNumber = rng.Next(methodDeckName.Count);
+                    Console.WriteLine($"{players[i].Name}, ваша карта {methodDeckName[randomDeckCardNumber].Rank} {methodDeckName[randomDeckCardNumber].Suit}");
+                    players[i].Score = players[i].Score + methodDeckName[randomDeckCardNumber].NumValue;
+                    methodDeckName.RemoveAt(randomDeckCardNumber);
+                   
+
+                    if (players[i].Score > 21)
+                    {
+                        Console.WriteLine($"{players[i].Name}, вы выбыли из игры, ваш итоговый счет - {players[i].Score}");
+                        
+                        players[i].Score = 0;
+                        playersInGame = playersInGame - 1;
+                        continue;
+
+                    }
+                    else if (players[i].Score == 21)
+                    {
+                        Console.WriteLine($"{players[i].Name}, вы победили, набрав 21 очко (молодец нахуй)");
+                        Console.ReadKey();
+                        playersInGame = playersInGame - 1;
+                        winners[i] = 21;
+                        players[i].Score = 0;
+                        continue;
+
+                    }
+                    
+                   
+
                 }
             }
         }
-        public static bool GettingUserChoice(string requestMessage)
+        public static bool GettingUserChoice(string userName, string requestMessage)
         {
             while (true)
             {
-                Console.WriteLine(requestMessage);
+                Console.WriteLine($"{userName}{requestMessage}");
                 string userAnswer = Console.ReadLine();
                 if (userAnswer.ToLower() == "да")
                 {
--- Player.cs
+++ Player.cs
@@ -3,4 +3,5 @@
 public class Player
 {
     public string Name { get; set; }
+    public int Score { get; set; }
 }
