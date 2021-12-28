bf96886f771e3a37dc7ba1b5acaf3b66c993d4bf
eduard <kuchmel.eded@mail.ru>
2021-12-28 20:43:15 +0300

    Add gamewinneers withdraw

--- OchkoGame.cs
+++ OchkoGame.cs
@@ -15,6 +15,7 @@ namespace MyApp
             //заполнение колоды двумя методами
             AddDifferentCardSuitsToDeck(deck);
             OchkoPlaying(deck);
+
             Console.ReadKey();
         }
         //Метод для создания колоды (задает численное значение и номинал карты)
@@ -45,31 +46,37 @@ namespace MyApp
             CardSuit = "крести";
             AddDifferentCardsToDeck(methodDeckName, CardSuit);
         }
+        //Метод для процесса вытягивания карт
+        public static void DrawCards(List<Card> methodDeckName, List<Player> PlayersList, int CycleCounter)
+        {
+            Random rng = new Random();
+            int randomDeckCardNumber = rng.Next(methodDeckName.Count);
+            Console.WriteLine($"{PlayersList[CycleCounter].Name}, ваша карта {methodDeckName[randomDeckCardNumber].Rank} {methodDeckName[randomDeckCardNumber].Suit}");
+            PlayersList[CycleCounter].Score = PlayersList[CycleCounter].Score + methodDeckName[randomDeckCardNumber].NumValue;
+            methodDeckName.RemoveAt(randomDeckCardNumber);
+        }
         //Игровой модуль
         public static void OchkoPlaying(List<Card> methodDeckName)
         {
             //ввод пользователей в игру
             List<Player> players = new List<Player>();
+            List<Player> winnersList = new List<Player>();
             Console.WriteLine("Введите количество игроков");
             int playerCount = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i <= playerCount - 1; i++)
             {
                 Console.WriteLine($"Введите имя {i + 1}-ого игрока");
-                players.Add(new Player() { Name = Console.ReadLine() });
+                winnersList.Add(new Player() { Name = Console.ReadLine() });
+                players.Add(new Player() { Name = winnersList[i].Name });
             }
 
-            //рандомизация раздач
-            Random rng = new Random();
+
             //пример предложения о вытягивании карты
             string requestMessageTemplate = ", хотите ли вы вытянуть еще одну карту?";
             int playersInGame = playerCount;
-            int[] winners = new int[playerCount];
             for (int i = 0; i < playerCount; i++)
             {
-                int randomDeckCardNumber = rng.Next(methodDeckName.Count);
-                Console.WriteLine($"{players[i].Name}, ваша карта {methodDeckName[randomDeckCardNumber].Rank} {methodDeckName[randomDeckCardNumber].Suit}");
-                players[i].Score = players[i].Score + methodDeckName[randomDeckCardNumber].NumValue;
-                methodDeckName.RemoveAt(randomDeckCardNumber);
+                DrawCards(methodDeckName, players, i);
             }
             while (playersInGame >= 1)
             {
@@ -84,22 +91,19 @@ namespace MyApp
                     if (!GettingUserChoice(players[i].Name, requestMessageTemplate))
                     {
                         Console.WriteLine($"{players[i].Name}, игра завершена, ваш итоговый счет - {players[i].Score}");
-                      
-                        winners[i] = players[i].Score;
+
+                        winnersList[i].Score = players[i].Score;
                         players[i].Score = 0;
                         playersInGame = playersInGame - 1;
                         continue;
                     }
-                    int randomDeckCardNumber = rng.Next(methodDeckName.Count);
-                    Console.WriteLine($"{players[i].Name}, ваша карта {methodDeckName[randomDeckCardNumber].Rank} {methodDeckName[randomDeckCardNumber].Suit}");
-                    players[i].Score = players[i].Score + methodDeckName[randomDeckCardNumber].NumValue;
-                    methodDeckName.RemoveAt(randomDeckCardNumber);
-                   
+                    DrawCards(methodDeckName, players, i);
+
 
                     if (players[i].Score > 21)
                     {
                         Console.WriteLine($"{players[i].Name}, вы выбыли из игры, ваш итоговый счет - {players[i].Score}");
-                        
+                        winnersList[i].Score = 0;
                         players[i].Score = 0;
                         playersInGame = playersInGame - 1;
                         continue;
@@ -110,16 +114,24 @@ namespace MyApp
                         Console.WriteLine($"{players[i].Name}, вы победили, набрав 21 очко (молодец нахуй)");
                         Console.ReadKey();
                         playersInGame = playersInGame - 1;
-                        winners[i] = 21;
+                        winnersList[i].Score = 21;
                         players[i].Score = 0;
                         continue;
 
                     }
-                    
-                   
+
+
 
                 }
             }
+            MassiveSorting(winnersList);
+            for (int i = 0; i < winnersList.Count; i++)
+            {
+                Console.Write(winnersList[i].Score + " ");
+            }
+            WithdrawingGameWinners(winnersList);
+
+
         }
         public static bool GettingUserChoice(string userName, string requestMessage)
         {
@@ -142,6 +154,45 @@ namespace MyApp
             }
 
         }
+        //сортировка массива
+        public static void MassiveSorting(List<Player> listName)
+        {
+            for (int i = 0; i < listName.Count - 1; i++)
+            {
+                for (int j = i + 1; j < listName.Count; j++)
+                {
+                    if (listName[i].Score < listName[j].Score)
+                    {
+                        int temp = listName[i].Score;
+                        listName[i].Score = listName[j].Score;
+                        listName[j].Score = temp;
+                    }
+                }
+            }
+        }
+        //вывод победителей игры
+        public static void WithdrawingGameWinners(List<Player> listname)
+        {
+            int i = 0;
+            while (true)
+            {
+                if (listname[i].Score == listname[i + 1].Score)
+                {
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Список победителей игры:");
+                    int winnersCount = 0;
+                    do
+                    {
+                        Console.WriteLine($"{listname[winnersCount].Name}, набравший {listname[winnersCount].Score} баллов");      //Знаю, что использовать do while нехорошо, но я очень хотел, 
+                        winnersCount++;                                                                                         //чтобы сначала выполнилось действие, а потом была проверка условия
+                    } while (winnersCount <= i);
+
+                    break;
+                }
+            }
+        }
     }
 }
-
