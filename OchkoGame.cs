30f27c66857cd4dabdc18baf067876b85a688de1
Maksim Andruhin <Maks.Andruhin@gmail.com>
2022-01-27 09:04:34 +0300

    Refactor code for working with XML

--- OchkoGame.cs
+++ OchkoGame.cs
@@ -1,11 +1,7 @@
-﻿using System.Xml;
-
-namespace MyApp
+﻿namespace MyApp
 {
     public class OchkoGame
     {
-        static string path = Directory.GetCurrentDirectory() + @"\\Players.xml";
-
         public static void Main(string[] args)
         {
             List<Card> deck = CardManager.GenerateDeck();
@@ -13,111 +9,28 @@ namespace MyApp
             Console.ReadKey();
         }
 
-        public static void CreateXmlFile()
-        {
-            XmlDocument xNewPlayersDocument = new XmlDocument();
-            XmlDeclaration xmlDec = xNewPlayersDocument.CreateXmlDeclaration("1.0", "utf-8", null);
-            xNewPlayersDocument.AppendChild(xmlDec);
-            XmlElement playersDatabase = xNewPlayersDocument.CreateElement("Database");
-            playersDatabase.SetAttribute("name", "Игроки");
-            xNewPlayersDocument.AppendChild(playersDatabase);
-            xNewPlayersDocument.Save(path);
-        }
-
-        //Игровой модуль
+        // Игровой модуль
         public static void OchkoPlaying(List<Card> deck)
         {
-            List<Player> players = new List<Player>();
+            XmlManager.CreateFile();
 
-            if (!File.Exists(path))
-            {
-                CreateXmlFile();
-            }
-
-            XmlDocument xPlayersDocument = new XmlDocument();
-            xPlayersDocument.Load(path);
-            int numberOfPlayers = ConsoleIOManager.GetNumberOfPlayers();
-            for (int i = 0; i < numberOfPlayers; i++)
-            {
-                players.Add(new Player(ConsoleIOManager.GetUserName(i)));
-                bool isPlayerExists = false;
-                XmlElement? xRoot = xPlayersDocument.DocumentElement;
-                foreach (XmlElement xnode in xRoot)
-                {
-                    XmlNode? attr = xnode.Attributes.GetNamedItem("name");
-                    if (attr.Value == players[i].Name)
-                    {
-                        isPlayerExists = true;
-                        break;
-                    }
-                }
-                if (!isPlayerExists)
-                {
-                    XmlElement player = xPlayersDocument.CreateElement("Игрок");
-                    player.SetAttribute("name", players[i].Name);
-                    player.SetAttribute("winCount", "0");
-                    xRoot.AppendChild(player);
-                }
-            }
-
-            xPlayersDocument.Save(path);
-
-            for (int i = 0; i < numberOfPlayers; i++)
-            {
-                players[i].DrawCard(deck);
-            }
-            GameProcess(deck, players);
-            DetermineWinners(players);
-        }
-
-        public static void OchkoPlayingWithoutXml(List<Card> deck)
-        {
             List<Player> players = new List<Player>();
             int numberOfPlayers = ConsoleIOManager.GetNumberOfPlayers();
 
-            for (int i = 0; i <= numberOfPlayers; i++)
-            {
-                players.Add(new Player(ConsoleIOManager.GetUserName(i)));
-            }
+            // TODO: честно - хз на сколько вщ грамотно так писать. Думаю, что не очень.
+            // TODO: Но я сейчас активно разбираюсь с LINQ, поэтому стараюсь все, что только можно писать с его помощью, практики ради.
+            // TODO: Ну и тебе полезно будет знать, что такая гразь тоже возможна, и она вполне себе работает)
+            // TODO: Ниже оставлю закомменченным код, который делает все то же самое, но по-человечески))
+            players = Enumerable.Range(0, numberOfPlayers).Select(i => new Player(ConsoleIOManager.GetUserName(i))).ToList();
+            //for (int i = 0; i < numberOfPlayers; i++)
+            //{
+            //    players.Add(new Player(ConsoleIOManager.GetUserName(i)));
+            //}
 
+            XmlManager.addPlayersToXml(players);
             players.ForEach(player => player.DrawCard(deck));
             GameProcess(deck, players);
             DetermineWinners(players);
-        }
-
-        //вывод победителей игры
-        public static void DetermineWinners(List<Player> playersList)
-        {
-            List<Player> winners = GetPlayersWithHighestScore(playersList);
-            ConsoleIOManager.PrintWinners(winners);
-            PrintWinnersToXml(winners);
-        }
-
-        public static void PrintWinnersToXml(List<Player> winners)
-        {
-            XmlDocument xPlayersDocument = new XmlDocument();
-            xPlayersDocument.Load(path);
-            XmlElement? xRoot = xPlayersDocument.DocumentElement;
-
-            foreach (Player winner in winners)
-            {
-                foreach (XmlElement xnode in xRoot)
-                {
-                    XmlNode? attr = xnode.Attributes.GetNamedItem("name");
-                    if (attr.Value == winner.Name)
-                    {
-                        XmlNode? winnerCount = xnode.Attributes.GetNamedItem("winCount");
-                        winnerCount.Value = Convert.ToString(Convert.ToInt16(winnerCount.Value) + 1);
-                        xPlayersDocument.Save(path);
-                    }
-                }
-            }
-        }
-
-        public static List<Player> GetPlayersWithHighestScore(List<Player> playersList)
-        {
-            int maxScore = playersList.Max(p => p.Score);
-            return playersList.FindAll(p => p.Score == maxScore);
         }
 
         // TODO: Пересмотри внимательно еще раз этот код. Его можно улучшить.
@@ -167,6 +80,20 @@ namespace MyApp
                     playersInGame--;
                 }
             }
+        }
+
+        //вывод победителей игры
+        public static void DetermineWinners(List<Player> playersList)
+        {
+            List<Player> winners = GetPlayersWithHighestScore(playersList);
+            ConsoleIOManager.PrintWinners(winners);
+            XmlManager.PrintWinners(winners);
+        }
+
+        public static List<Player> GetPlayersWithHighestScore(List<Player> playersList)
+        {
+            int maxScore = playersList.Max(p => p.Score);
+            return playersList.FindAll(p => p.Score == maxScore);
         }
     }
 }
---
+++ XmlManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Xml.Linq;
+
+public class XmlManager
+{
+    private static readonly string fullPath = Directory.GetCurrentDirectory() + @"\\Players.xml";
+
+    private const string rootElementName = "Players";
+    private const string playerElementName = "Player";
+
+    public static void CreateFile()
+    {
+        if (!File.Exists(fullPath))
+        {
+            XDeclaration declaration = new XDeclaration("1.0", "utf-8", "yes");
+            XElement rootElement = new XElement(rootElementName);
+            XDocument xDoc = new XDocument(declaration, rootElement);
+            xDoc.Save(fullPath);
+        }        
+    }
+
+    public static void addPlayersToXml(List<Player> players)
+    {
+        XDocument? xDoc = XDocument.Load(fullPath);
+        XElement? xPlayersElement = xDoc.Descendants(rootElementName).First();
+
+        foreach (Player player in players)
+        {
+            if (isPlayerExists(xPlayersElement, player))
+            {
+                increasePlayerStats(xPlayersElement, player, "Games");
+            }
+            else
+            {
+                createPlayer(xPlayersElement, player);
+            }
+        }
+
+        xDoc.Save(fullPath);
+    }
+
+    public static void PrintWinners(List<Player> winners)
+    {
+        XDocument? xDoc = XDocument.Load(fullPath);
+        XElement? xPlayersElement = xDoc.Descendants(rootElementName).First();
+
+        winners.ForEach(winner => increasePlayerStats(xPlayersElement, winner, "Wins"));
+
+        xDoc.Save(fullPath);
+    }
+
+    private static void increasePlayerStats(XElement xPlayersElement, Player player, string fieldName)
+    {
+        XElement xPlayerElement = xPlayersElement.Elements(playerElementName)
+            .FirstOrDefault(p => p.Attribute("Name").Value == player.Name);
+
+        int fieldOldValue = int.Parse(xPlayerElement.Element(fieldName).Value);
+
+        xPlayerElement.SetElementValue(fieldName, fieldOldValue + 1);
+    }
+
+    private static bool isPlayerExists(XElement xPlayersElement, Player player)
+    {
+        return xPlayersElement.Descendants(playerElementName)
+            .Where(p => p.Attribute("Name").Value == player.Name).Any();
+    }
+
+    private static void createPlayer(XElement xPlayersElement, Player player)
+    {
+        XElement xPlayerElement = new XElement(playerElementName, new XAttribute("Name", player.Name));
+        XElement xPlayerGamesElement = new XElement("Games", 1);
+        XElement xPlayerWinsElement = new XElement("Wins", 0);
+
+        xPlayerElement.Add(xPlayerGamesElement, xPlayerWinsElement);
+        xPlayersElement.Add(xPlayerElement);
+    }
+}
